f6374430f455b76600883a061f5c23da38683baf
dsen <dgncnsn@hotmail.com>
2022-01-20 15:13:24 +0300

    User Server initial commit

---
+++ UserServer/src/test/java/com/dsen/UserServer/UserServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsen.UserServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
