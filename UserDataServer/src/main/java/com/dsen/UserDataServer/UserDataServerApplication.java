1aea7c6865483dc2dc034813e6930ab86d40ddfc
dsen <dgncnsn@hotmail.com>
2022-01-20 18:02:50 +0300

    UserDataServer initial commit

---
+++ UserDataServer/src/main/java/com/dsen/UserDataServer/UserDataServerApplication.java
@@ -0,0 +1,13 @@
+package com.dsen.UserDataServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDataServerApplication.class, args);
+	}
+
+}
---
+++ UserDataServer/src/test/java/com/dsen/UserDataServer/UserDataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsen.UserDataServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
