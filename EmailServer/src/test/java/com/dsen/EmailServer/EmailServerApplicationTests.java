ae848faa225e376bf53f9b7203da11a898358664
dsen <dgncnsn@hotmail.com>
2022-01-20 17:38:01 +0300

    EmailServer initial commit

---
+++ EmailServer/src/test/java/com/dsen/EmailServer/EmailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsen.EmailServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
